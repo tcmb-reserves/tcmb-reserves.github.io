d315c90f9cbcf3f78d83d4837ddb74bc1a225ea7
aiasci <aiasci@outlook.com>
2021-06-10 09:25:39 +0300

    commit

---
+++ 00tester.R
@@ -0,0 +1,27 @@
+
+
+
+
+library(textreadr)
+
+library(data.table)
+
+library(tidytext)
+
+library(dplyr)
+
+
+a <- data.table(read_pdf("https://www.tcmb.gov.tr/wps/wcm/connect/cc755e33-b5c0-4632-bfe4-2434d35da011/RT20210326TR.pdf?MOD=AJPERES&CACHEID=ROOTWORKSPACE-cc755e33-b5c0-4632-bfe4-2434d35da011-nzliV18"))
+
+yswap <- a %>%
+  unnest_tokens(word, text)
+
+yswap <- yswap[page_id == 1]
+
+yswap$rown <- c(1:nrow(yswap))
+
+yswap[, word := gsub(".", "", word, fixed = TRUE)]
+
+as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+5])
+as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+6])
+
