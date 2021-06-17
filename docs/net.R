55a87a81de2f3f141b3e71bd2aa25eac5636fb24
Ömer Taha <omertahagulberk@hotmail.com>
2021-06-17 14:09:43 +0300

    c
    c

--- docs/net.R
+++ docs/net.R
@@ -196,9 +196,9 @@ yswap$rown <- c(1:nrow(yswap))
 
 yswap[, word := gsub(".", "", word, fixed = TRUE)]
 
-toplam <- as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+5])
+toplam <- as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+n1])
 
-diger <- as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+19])
+diger <- as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+n2])
 
 val <- toplam + diger
 
