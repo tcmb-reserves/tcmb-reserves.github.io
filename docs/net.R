0e12ecfa5cf3f135d5d7c0cfdfa433cbcc3212ff
aiasci <aiasci@outlook.com>
2021-08-12 14:30:33 +0300

    commit

--- docs/net.R
+++ docs/net.R
@@ -196,9 +196,9 @@ yswap$rown <- c(1:nrow(yswap))
 
 yswap[, word := gsub(".", "", word, fixed = TRUE)]
 
-toplam <- as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+ 4])
+toplam <- as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+ xxx])
 
-diger <- as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+ 18])
+diger <- as.numeric(yswap$word[yswap[ word == "kapsar"]$rown+ yyy])
 
 val <- toplam + diger
 
